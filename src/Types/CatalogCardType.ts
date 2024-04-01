aa0d8cb2599879342f47e7b160f026ba153f5f93
macman <vasily-bazhenov.03@mail.ru>
2024-04-01 19:29:17 +0300

    first commit

---
+++ src/Types/CatalogCardType.ts
@@ -0,0 +1,13 @@
+export readonly type CatalogCardType = {
+    id: number | string,
+    material: number,
+    name: string,
+    price: {
+        old_price?: number,
+        current_price: number
+    },
+    image: {
+        url: string
+    },
+    code?: string
+}
---
+++ src/components/CatalogCard.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="card">
+		<img :src="`@img/${card.image.url}`" :alt="`product №${card.id}`" class="card_image">
+		<div class="card__head">
+			span.
+		</div>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { CatalogCardType } from "../Types/CatalogCardType.ts";
+
+interface IProps {
+	card: CatalogCardType,
+}
+
+const props = defineProps<IProps>({
+	card: {
+		type: CatalogCardType,
+		required: true,
+	}
+})
+</script>
+
+<style scoped lang="scss">
+</style>
